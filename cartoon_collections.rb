d2730e8b96131e31dd37e7c087f6b3fc29800d57
Alex S Grimes <alex@alexgrimes.com>
2020-04-27 20:29:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,9 @@
+
 def greet_characters(array)
+  array.each do |element| 
+  puts "Hello #{element}!"
+end 
+  
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
@@ -8,4 +13,8 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |element, index|
+    puts "#{index + 1}. #{element}"
+  end 
 end
+
